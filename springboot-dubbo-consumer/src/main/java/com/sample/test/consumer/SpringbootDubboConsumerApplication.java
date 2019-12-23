de36a54026ba6d447e9a351babffa1505c56beff
jack-huang2016 <2456120975@qq.com>
2019-12-23 18:36:29 +0800

    springboot整合dubbo

--- springboot-dubbo-consumer/src/main/java/com/sample/test/consumer/SpringbootDubboConsumerApplication.java
+++ springboot-dubbo-consumer/src/main/java/com/sample/test/consumer/SpringbootDubboConsumerApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringbootDubboConsumerApplication {
 
+    // 启动的顺序是必须先启动Zookeeper注册中心，再启动服务提供方的模块，再启动消费者的模块，不然会报错
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDubboConsumerApplication.class, args);
     }
